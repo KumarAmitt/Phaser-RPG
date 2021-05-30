9a017c17282a41e693d17cbe378631e37239f347
KumarAmitt <amitt2015@outlook.com>
2021-05-30 22:57:38 +0530

    Add model.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import PreloaderScene from './js/Scenes/PreloaderScene.js';
 import TitleScene from './js/Scenes/TitleScene.js';
 import OptionsScene from './js/Scenes/OptionsScene.js';
 import CreditsScene from './js/Scenes/CreditsScene.js';
-import Model from './Model.js';
+import Model from './js/Model.js';
 
 class Game extends Phaser.Game {
   constructor() {

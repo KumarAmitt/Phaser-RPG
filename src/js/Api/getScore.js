90bd4f284f451aa782afe273b675dce3fa6fcb2e
KumarAmitt <amitt2015@outlook.com>
2021-06-01 18:44:00 +0530

    Add getScore function

---
+++ src/js/Api/getScore.js
@@ -0,0 +1,22 @@
+import {URL, APIkey} from "./constants";
+
+const sorter = (object) => object.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+const getScores = async () => {
+  try {
+    const res = await fetch(`${URL}/games/${APIkey}/scores`, {
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    const { result } = await res.json();
+    const sortedResult = sorter(result);
+    console.log(sortedResult.slice(0, 9));
+    return sortedResult.slice(0, 9);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default

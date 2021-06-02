7da8a4d344b56c0571a28ad87d7517f36b27a17a
KumarAmitt <amitt2015@outlook.com>
2021-06-02 15:03:59 +0530

    Extract sorter function

--- src/js/Api/getScore.js
+++ src/js/Api/getScore.js
@@ -1,6 +1,5 @@
 import {URL, APIkey} from "./constants";
-
-const sorter = (object) => object.sort((a, b) => (a.score > b.score ? -1 : 1));
+import { sorter } from '../Helper/sorter'
 
 const getScores = async () => {
   try {
@@ -13,8 +12,7 @@ const getScores = async () => {
     const { result } = await res.json();
     const sortedResult = sorter(result);
 
-    return result
-    // return sortedResult.slice(0, 10);
+    return sortedResult.slice(0, 10);
   } catch (error) {
     throw new Error(error);
   }
---
+++ src/js/Helper/sorter.js
@@ -0,0 +1,1 @@
+export const sorter = (object) => object.sort((a, b) => (a.score > b.score ? -1 : 1));

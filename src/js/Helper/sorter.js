e9ba85d172693a6aa28960a73f270b8a9bcfbf0d
KumarAmitt <amitt2015@outlook.com>
2021-06-02 15:57:49 +0530

    Fix eslint offense in sorter.js

--- src/js/Helper/sorter.js
+++ src/js/Helper/sorter.js
@@ -1,1 +1,3 @@
-export const sorter = (object) => object.sort((a, b) => (a.score > b.score ? -1 : 1));
+const sorter = (object) => object.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+export default sorter;
